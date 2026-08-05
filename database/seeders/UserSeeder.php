f727d7bcd459e4247ad928a9a72eeb78e5b4bc3c
KevinAdhaikal <adhaikalk@gmail.com>
2026-08-06 00:07:50 +0700

    feat: update endpoint and fix admin path

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Administrator',
+            'username' => 'admin',
+            'email' => 'admin@cimulang.desa.id',
+            'password' => Hash::make("admin"),
+            'email_verified_at' => now()
+        ]);
+    }
+}
